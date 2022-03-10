d51e85384d5e9b29d9b8b16475cdf7dc893e98e0
Ball Panupan <Ball@reverseads.com>
2022-03-10 13:15:33 +0700

    update state

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
-            ...state,
-            numOfCakes: state.numOfCakes - 1
+            ...state, numOfCakes: state.numOfCakes - 1
         }
 
         default : return state
