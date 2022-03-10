36901bf5b98ea7b9dc16d112de8c323367018577
Ball Panupan <Ball@reverseads.com>
2022-03-10 11:09:18 +0700

    create action

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function buyCake(){
         info: 'First redux action' 
     }
 }
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default : return state
+    }
+}
