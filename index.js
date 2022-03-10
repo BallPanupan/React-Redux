4de14502005ea4cde8cb316ef07f5791727f7f97
Ball Panupan <Ball@reverseads.com>
2022-03-10 11:01:10 +0700

    update comment

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ console.log('form index.js')
 /*
     a "store" that holds the state of your application
     An "action" that describes the changes in the state of the application
-    A "reducer" whuch actually carries out the state transition depending on the action
+    A reducer" which actually carries out the state transition depending on the action
 */
